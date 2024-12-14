7311fd373b73e8518249bf08a1e545b317fa069a
Simon35845 <DrakeKingman1991@gmail.com>
2024-12-15 02:25:07 +0300

    commit_7.4 - созданы энтити-классы по стратегии Table Per Concrete class mapping (Single table)

---
+++ src/main/java/myGroupId51/jd2_homework_7/entity/Task.java
@@ -0,0 +1,40 @@
+package myGroupId51.jd2_homework_7.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Column;
+import javax.persistence.DiscriminatorColumn;
+import javax.persistence.DiscriminatorType;
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.Table;
+import java.io.Serializable;
+
+@SuperBuilder
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "TYPE_TASK", discriminatorType = DiscriminatorType.CHAR)
+@DiscriminatorValue("T")
+public class Task implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+}
