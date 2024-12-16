0e7c587b43dc6896fbbae2b2884d1029709a9e18
Simon35845 <DrakeKingman1991@gmail.com>
2024-12-17 01:36:38 +0300

    commit_5.8 - изменен main-класс AppEmployee для работы с новыми утилитными классами

--- src/main/java/myGroupId51/jd2_homework_5/AppEmployee.java
+++ src/main/java/myGroupId51/jd2_homework_5/AppEmployee.java
@@ -1,20 +1,14 @@
 package myGroupId51.jd2_homework_5;
 
-import myGroupId51.jd2_homework_5.Utils.JDBCResources;
-import myGroupId51.jd2_homework_5.dao.EmployeeDAO;
+import myGroupId51.jd2_homework_5.Utils.EmployeeTable;
 import myGroupId51.jd2_homework_5.dao.EmployeeDAOImpl;
 import myGroupId51.jd2_homework_5.dto.Employee;
 
-import java.sql.Date;
 import java.time.LocalDate;
 
 public class AppEmployee {
     public static void main(String[] args) {
-        String url = JDBCResources.getURL();
-        String user = JDBCResources.getUser();
-        String password = JDBCResources.getPassword();
-
-        EmployeeDAOImpl employeeDAO = new EmployeeDAOImpl(url, user, password);
+        EmployeeDAOImpl employeeDAO = new EmployeeDAOImpl();
         employeeDAO.createTable();
 
         Employee employee1 = Employee.builder()
@@ -53,7 +47,7 @@ public class AppEmployee {
 
         Employee employee5 = Employee.builder()
                 .firstName("Дмитрий")
-                .lastName("Вольтович")
+                .lastName("Вольтов")
                 .department("Покрасочный цех")
                 .salary(1594.44)
                 .hireDate(LocalDate.of(2016, 4, 17))
@@ -64,17 +58,20 @@ public class AppEmployee {
         employeeDAO.save(employee3);
         employeeDAO.save(employee4);
         employeeDAO.save(employee5);
-
         employee2.setDepartment("Сборочный цех");
         employee2.setSalary(1924.25);
-        employeeDAO.update(employee2);
+        employeeDAO.update(employee2, 2);
         employeeDAO.delete(5);
 
-        System.out.println(employeeDAO.find(3));
-        employeeDAO.findAll().forEach(System.out::println);
-        employeeDAO.findByDepartment("Сборочный цех").forEach(System.out::println);
-        employeeDAO.findBySalary(1700).forEach(System.out::println);
-        employeeDAO.findAllOrderBySalary().forEach(System.out::println);
-        employeeDAO.findAllOrderByLastName().forEach(System.out::println);
+        System.out.print("Условие: id = 3.\n");
+        EmployeeTable.printOneResult(employeeDAO.find(3));
+        EmployeeTable.printManyResults(employeeDAO.findAll());
+        System.out.print("\nУсловие: отдел = \"Сборочный цех\".\n");
+        EmployeeTable.printManyResults(employeeDAO.findByDepartment("Сборочный цех"));
+        System.out.print("\nУсловие: зарплата > 1700 р.\n");
+        EmployeeTable.printManyResults(employeeDAO.findBySalary(1700));
+        EmployeeTable.printManyResults(employeeDAO.findAllOrderBySalary());
+        EmployeeTable.printManyResults(employeeDAO.findAllOrderByLastName());
     }
 }
+
